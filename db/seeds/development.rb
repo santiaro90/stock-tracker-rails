89ad9cac7dc86ae7bb61e9c2943a85134faa2875
Santiago Alvarez Rodriguez <santiaro90@gmail.com>
2018-02-21 16:15:14 -0500

    Add seed users in dev env

---
+++ db/seeds/development.rb
@@ -0,0 +1,29 @@
+User.create(email: 'santiago@example.com',
+            first_name: 'Santiago',
+            last_name: 'Alvarez',
+            password: 'password',
+            password_confirmation: 'password')
+
+User.create(email: 'geral@example.com',
+            first_name: 'Geraldine',
+            last_name: 'Lopez',
+            password: 'password',
+            password_confirmation: 'password')
+
+User.create(email: 'amparo@example.com',
+            first_name: 'Amparo',
+            last_name: 'Rodriguez',
+            password: 'password',
+            password_confirmation: 'password')
+
+User.create(email: 'rodrigo@example.com',
+            first_name: 'Rodrigo',
+            last_name: 'Alvarez',
+            password: 'password',
+            password_confirmation: 'password')
+
+User.create(email: 'mariana@example.com',
+            first_name: 'Mariana',
+            last_name: 'Zapata',
+            password: 'password',
+            password_confirmation: 'password')
